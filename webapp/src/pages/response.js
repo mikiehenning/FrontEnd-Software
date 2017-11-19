11b0fda3babf7b23f031d770c02bb7e0b1963369
MarleyKreps <33332461+MarleyKreps@users.noreply.github.com>
2017-11-19 16:03:06 -0500

    11/19/17
    3:46pm fixed wagnerScaleTest, styling link is displacing the div containing the test itself, while not touching the logo div.

--- webapp/src/pages/response.js
+++ webapp/src/pages/response.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class response extends Component {
     render(){
         return (
-            <label>Wound Feedback: {this.props.info}</label>
+            <h1>Wound Feedback: {this.props.info}</h1>
         );
     };
 }
